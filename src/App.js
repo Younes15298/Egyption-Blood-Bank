08b6bcbf1d96d9471caa6e241f0f0ced1df6b1e3
Mohamed Younes <83447869+Younes15298@users.noreply.github.com>
2022-03-11 03:33:25 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./css/ebb.css";
+import Register from "./register";
+import React from "react";
+
+function App() {
+  return <Register />;
+}
+
+export default App;

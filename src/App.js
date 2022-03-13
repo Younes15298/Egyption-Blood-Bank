e0e81d86576fb5b3cecc327464db172b634ca005
Younes15298 <mohamed15298a7@gmail.com>
2022-03-13 22:42:46 +0200

    new

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Register from "./components/register/register";
+// import Login from "./components/login/login";
+
+function App() {
+  return <Register />;
+}
+
+export default App;
